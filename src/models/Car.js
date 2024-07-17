14c65da4c1fa9dbdd614dbb342fb44a5abb1fe42
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-17 09:45:54 -0600

    created car model

---
+++ src/models/Car.js
@@ -0,0 +1,17 @@
+export class Car {
+  constructor(data) {
+    this.id = data.id || data._id
+    this.make = data.make
+    this.model = data.model
+    this.imgUrl = data.imgUrl
+    this.year = data.year
+    this.price = data.price
+    this.description = data.description
+    this.color = data.color
+    this.engineType = data.engineType
+    this.creatorId = data.creatorId
+    this.creator = data.creator
+    this.createdAt = new Date(data.createdAt)
+    this.updatedAt = new Date(data.updatedAt)
+  }
+}
